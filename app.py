a944c1c14c2f5767e430a9a0ce1a7d6448d4e9f8
sandroaryabima123-lgtm <sandroaryabima123@gmail.com>
2026-02-12 20:46:37 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 from flask import Flask, request, send_file
 from flask_cors import CORS
-from rembg import remove
+from rembg import remove, new_session
 import io
 import os
 
 app = Flask(__name__)
-CORS(app) # Penting agar frontend bisa mengakses backend
+CORS(app)
+
+# Buat session khusus agar lebih stabil
+session = new_session("u2netp") # Versi 'u2netp' lebih ringan dari default
 
 @app.route('/remove-bg', methods=['POST'])
 def remove_bg():
     if 'image_file' not in request.files:
-        return "No file uploaded", 400
+        return "No file", 400
+    
     file = request.files['image_file'].read()
-    output = remove(file)
+    
+    # Proses hapus background dengan session ringan
+    output = remove(file, session=session)
+    
     return send_file(io.BytesIO(output), mimetype='image/png')
 
 if __name__ == '__main__':
